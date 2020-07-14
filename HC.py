b30f7210a7760de40da419ac73b3d756b565df98
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-14 14:43:21 +0100

    Initial project scaffolding and libraries, modules and packages importation

---
+++ HC.py
@@ -0,0 +1,14 @@
+#=====================================IMPORTING NEEDED LIBRARIES=====================================
+import numpy as np 
+import pandas as pd
+from scipy import ndimage 
+from scipy.cluster import hierarchy 
+from scipy.spatial import distance_matrix 
+from matplotlib import pyplot as plt 
+from sklearn import manifold, datasets 
+from sklearn.cluster import AgglomerativeClustering 
+from sklearn.datasets.samples_generator import make_blobs 
+#%matplotlib inline #useful in jupyter notebooks
+
+
+#=======================================
