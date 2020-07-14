1eef1038e2cfde83004ae64e108d1c2d3a64f5bf
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-14 15:06:11 +0100

    Data Processing: Data cleaning

--- HC.py
+++ HC.py
@@ -20,3 +20,17 @@ df = pdf.head(5)
 print(df)
 
 
+#============================================DATA PROCESSING=============================================
+
+#=============================================data cleaning=======================================
+print ("Shape of dataset before cleaning: ", pdf.size)
+pdf[[ 'sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']] = pdf[['sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']].apply(pd.to_numeric, errors='coerce')
+pdf = pdf.dropna()
+pdf = pdf.reset_index(drop=True)
+print ("Shape of dataset after cleaning: ", pdf.size)
+pdf.head(5)
+
